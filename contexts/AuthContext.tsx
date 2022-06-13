053fbfab132eb33fdd91e546e3c84461189fa3c6
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-13 10:38:26 -0300

    retrieving authentication state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import Router from "next/router";
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
+import { setCookie, parseCookies } from 'nookies'
 
 type User = {
     email: string;
@@ -29,6 +30,18 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     const [user, setUser] = useState<User>()
     const isAuthenticated = !!user
 
+    useEffect(() => {
+        const { 'nextauth.token': token } = parseCookies()
+
+        if (token) {
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data
+
+                setUser({ email, permissions, roles })
+            })
+        }
+    }, [])
+
     const signIn = async ({ email, password }: SignInCredendials) => {
         try {
             const response = await api.post('sessions', {
@@ -36,13 +49,22 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
                 password
             })
 
-            const { permissions, roles } = response.data
+            const { token, refreshToken, permissions, roles } = response.data
+
+            // o primeiro parâmetro é sempre undefined, porque o signIn é uma ação do usuário feita no browser.
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // (30 days) quanto tempo o cookie deve ser mantido no navegador
+                path: '/' // qualquer endereço da app vai ter acesso ao cookie, geralmente usado '/' para um cookie global
+            })
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken)
 
             setUser({
                 email,
                 permissions,
                 roles
             })
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
 
             Router.push('/dashboard')
         } catch (err) {
